9ba66381fb17898e8d54ad7cd0457945373be49a
James Monks <J.monks23@gmail.com>
2020-12-14 21:44:26 +0000

    had to add script into index.html to fix an uncaught 'referenceError: global is not defined' app is now working and displaying items in browser and saving items to the database

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import Amplify from "aws-amplify";
-import aws_exports from "./aws-exports";
+import aws_exports from "./aws-exports.js";
 
 Amplify.configure(aws_exports);
 
